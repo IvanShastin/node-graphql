398845b4a846732da366d09ea9d71daebe509303
Ivan Shastin <ivan.shastin@concur.com>
2018-03-16 10:52:39 +0100

    working GraphQL

---
+++ data/schema.js
@@ -0,0 +1,65 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { resolvers } from './resolvers';
+
+
+const typeDefs = `
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String
+        contacts: [Contact]
+    }
+
+    type Contact {
+        firstName: String
+        lastName: String
+    }
+
+    type Alien {
+        id: ID
+        firstName: String
+        lastName: String
+        planet: String
+    }
+
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
+    }
+
+    type Query {
+        getOneFriend(id: ID!): Friend
+        getAliens: [Alien]
+    }
+
+    input ContactInput {
+        firstName: String
+        lastName: String
+    }
+
+    input FriendInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String
+        contacts: [ContactInput]
+    }
+
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+        updateFriend(input: FriendInput): Friend
+        deleteFriend(id: ID): String
+    }
+`;
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
+export {schema};
